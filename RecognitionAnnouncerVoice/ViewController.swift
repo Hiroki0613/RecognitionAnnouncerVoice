8e85394370f57304a036ec889d95a749ed456c0f
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-09 16:02:09 +0900

    音声認識の許可を実装,NHKの音声を追加

--- RecognitionAnnouncerVoice/ViewController.swift
+++ RecognitionAnnouncerVoice/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,SFSpeechRecognizerDelegate {
+    
+    // MARK: 温泉関連Properties
+    //localeのidentifierに言語を指定、。日本語はja-JP,英語はen-US
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        speechRecognizer?.delegate = self
+        
+        //ユーザーに音声認識の許可を求める
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    //ユーザが音声認識の許可を出した時
+                    print("音声認識許可")
+                case .denied:
+                    //ユーザが音声認識を拒否した時
+                    print("音声認識拒否")
+                case .restricted:
+                    //端末が音声認識に対応していない場合
+                    print("音声認識が対応していません")
+                case .notDetermined:
+                    //ユーザが音声認識をまだ認証していない時
+                    print("音声認識の認証ができていません")
+                }
+            }
+        }
     }
 
+    
+    
 
 }
 
